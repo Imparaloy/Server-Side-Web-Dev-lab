824fa66510679c9e36e2e46038702a020ac09a72
Sirikul Khummung <66070192@kmitl.ac.th>
2025-08-13 19:08:01 +0700

    add API Document

---
+++ routes/webRoutes.js
@@ -0,0 +1,10 @@
+// routes/webRoutes.js
+const express = require('express');
+const router = express.Router();
+
+const db = require('../config/db');
+const productController = require('../controllers/productController');
+
+router.get('/', productController.getProductsView);
+
+module.exports = router;
