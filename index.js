6aa5610b1ac74115133ce15970ce356f7dca304e
Sirikul Khummung <66070192@kmitl.ac.th>
2025-08-13 19:18:53 +0700

    fix swagger

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+
 const app = express();
+
 const PORT = 3000;
-const productRoutes = require('./routes/productRoutes.js');
 
-app.use(express.json());
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+const path = require('path');
 
+const productRoutes = require('./routes/productRoutes');
+const webRoutes = require('./routes/webRoutes');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json())
 app.use('/api', productRoutes);
+app.use('/', webRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
 
